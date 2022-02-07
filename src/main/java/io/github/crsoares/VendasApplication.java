7ea122f6ec002e5a779c558a417f0aa7f6d10d99
crsoares <crysthianophp@gmail.com>
2022-02-07 12:09:10 -0300

    new version

--- src/main/java/io/github/crsoares/VendasApplication.java
+++ src/main/java/io/github/crsoares/VendasApplication.java
@@ -34,10 +34,10 @@ public class VendasApplication {
             System.out.println("Buscando Clientes");
             clientes.buscarPorNome("Cli").forEach(System.out::println);
 
-//            System.out.println("Deletando Clientes");
-//            clientes.obterTodos().forEach(c -> {
-//                clientes.deletar(c);
-//            });
+            System.out.println("Deletando Clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
 
             todosClientes = clientes.obterTodos();
             if (todosClientes.isEmpty()) {
